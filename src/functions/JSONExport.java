da46ae7d077d75cf84f77a177153360523dc081b
Gorkagi <gorka.gonzalo@alumni.mondragon.edu>
2016-12-01 09:59:18 +0100

    CSV Export

--- src/functions/JSONExport.java
+++ src/functions/JSONExport.java
@@ -22,7 +22,7 @@ public class JSONExport {
 		obj.put("Company List", company);
 
 		try {
-			FileWriter file = new FileWriter("/file1.json");
+			FileWriter file = new FileWriter("file1.json");
 			file.write(obj.toJSONString());
 			System.out.println("Successfully Copied JSON Object to File...");
 			System.out.println("\nJSON Object: " + obj);
