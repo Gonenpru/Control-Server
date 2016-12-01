ca0b8a943380f399fff9457d8bd5ed5d69918e5e
Gorkagi <gorka.gonzalo@alumni.mondragon.edu>
2016-12-01 09:41:33 +0100

    JSON Export Functions

--- src/functions/JSONExport.java
+++ src/functions/JSONExport.java
@@ -1,5 +1,35 @@
 package functions;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class JSONExport {
 
+	public void CrunchifyJSONFileWrite(){
+
+
+		JSONObject obj = new JSONObject();
+		obj.put("Name", "crunchify.com");
+		obj.put("Author", "App Shah");
+
+		JSONArray company = new JSONArray();
+		company.add("Compnay: eBay");
+		company.add("Compnay: Paypal");
+		company.add("Compnay: Google");
+		obj.put("Company List", company);
+
+		try {
+			FileWriter file = new FileWriter("/file1.json");
+			file.write(obj.toJSONString());
+			System.out.println("Successfully Copied JSON Object to File...");
+			System.out.println("\nJSON Object: " + obj);
+		}catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
 }
+
+
